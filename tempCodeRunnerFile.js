ac6adfefd7e26452f99e418dcccc8215faf5a5de
Filza Siddiqui <115334313+filza2112@users.noreply.github.com>
2025-07-13 11:13:38 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const focusRoutes = require("./routes/focus");
+const moodRoutes = require("./routes/mood"); 
+const routineRoutes = require("./routes/routine");
+const quizRoutes = require("./routes/quiz"); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+app.use("/api/focus", focusRoutes);
+app.use("/api/mood", moodRoutes);
+app.use("/api/tasks", routineRoutes);
+app.use("/api/quiz", quizRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
