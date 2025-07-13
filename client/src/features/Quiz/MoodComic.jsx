d10426b0db83bd5b6b61bfb15889762870d639b4
Filza Siddiqui <115334313+filza2112@users.noreply.github.com>
2025-07-13 10:39:30 +0530

    Delete client directory

--- client/src/features/Quiz/MoodComic.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MoodComic = () => {
-  return (
-    <div className="p-6 text-center text-xl">🎨 Mood Comic Coming Soon...</div>
-  );
-};
-
-export default MoodComic;
